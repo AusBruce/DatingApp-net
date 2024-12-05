be78955b96c2899c9670186c68ca3a1288aa3fc7
AusBruce <wangyuheng086@gmail.com>
2024-12-05 18:06:28 +1100

    end of section 5 angular concept

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit,inject } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  
+  http = inject (HttpClient);
+  users: any;
+  registerMode = false;
+
+ngOnInit(): void {
+    
+  this.getUsers();
+}
+
+ cancelRegisterMode(event: boolean){
+  this.registerMode = event
+ }
+
+getUsers(){
+     
+  this.http.get('https://localhost:5001/api/users').subscribe({
+    next: response => this.users =response,
+    error: error => console.log(error),
+    complete: () => console.log('Request has completed')
+  })
+
+}
+
+  registerToggle(){
+    this.registerMode = !this.registerMode
+  }
+}
+
+
+
