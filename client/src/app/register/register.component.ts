0986250a850f0cecf4ca1b038fc5b76ff8efd7f6
AusBruce <wangyuheng086@gmail.com>
2024-12-06 20:10:07 +1100

    end of section 6 change UI and Angular routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input,  output,  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService =inject(AccountService);
-
+private toastr = inject(ToastrService)
 
 cancelRegister = output<boolean>();
 
@@ -25,7 +26,7 @@ cancelRegister = output<boolean>();
         console.log(response);
         this.cancel();
       },
-      error: error =>console.log(error)
+      error: error =>this.toastr.error(error.error)
     })
   }
 
