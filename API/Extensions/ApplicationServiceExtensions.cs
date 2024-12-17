190909ae801e6ad70bfd17e49e172bbffc01218b
AusBruce <wangyuheng086@gmail.com>
2024-12-17 15:28:34 +1100

    end of section 8,AutoMapper,repository pattern ,ef relations ,seeding data

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -22,6 +22,10 @@ public static class ApplicationServiceExtensions
 
         services.AddScoped<ITokenService, TokenService>();
 
+        services.AddScoped<IUserRepository,UserRepository>();
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
         return services;
     }
 }
