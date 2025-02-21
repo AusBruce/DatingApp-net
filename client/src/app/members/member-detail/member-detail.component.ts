8ac4ca4c84c067268cfd30189d6cfa1ecd29de39
AusBruce <wangyuheng086@gmail.com>
2025-02-21 15:22:22 +1100

    send message use hub

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
@@ -10,6 +10,7 @@ import { MemberMessagesComponent } from "../member-messages/member-messages.comp
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { PresenceService } from '../../_services/presence.service';
+import { AccountService } from '../../_services/account.service';
 
 @Component({
     selector: 'app-member-detail',
@@ -18,16 +19,18 @@ import { PresenceService } from '../../_services/presence.service';
     styleUrl: './member-detail.component.css',
     imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent]
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit ,OnDestroy{
   @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
   private messageService = inject(MessageService);
   private memberService = inject(MembersService);
+
+  private accountService = inject(AccountService);  
   presenceService = inject(PresenceService);
   private route = inject(ActivatedRoute);
   member: Member = {} as Member;
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
-  messages: Message[] = [];
+
 
   ngOnInit(): void {
     this.route.data.subscribe({
@@ -45,11 +48,7 @@ export class MemberDetailComponent implements OnInit {
       }
     })
   }
-  
-  onUpdateMessages(event: Message) {
-    this.messages.push(event);
-  }
-  
+
   
   selectTab(heading: string) {
     if (this.memberTabs) {
@@ -59,26 +58,16 @@ export class MemberDetailComponent implements OnInit {
   }
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
-    if (this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member) {
-      this.messageService.getMessageThread(this.member.userName).subscribe({
-        next: messages => this.messages = messages
-      })
+    if (this.activeTab.heading === 'Messages' && this.member) {
+      const user = this.accountService.currentUser();
+      if(!user) return;
+      this.messageService.createHubConnection(user, this.member.userName);
+    }else{
+      this.messageService.stopHubConnection();
     }
   }
 
-//   loadMember(){
-//     const username = this.route.snapshot.paramMap.get('username');
-//     if(!username) return;
-
-// this.memberService.getMember(username).subscribe({
-//       next: member => {
-//         this.member = member;
-//         member.photos.map(p => {
-//           this.images.push(new ImageItem({src: p.url, thumb: p.url}))
-//         })
-
-//         }
-//     })
-//   }
-
+ngOnDestroy(): void {
+    
+}
 }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -12,19 +12,18 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
+ messageService = inject(MessageService);
   
-  private messageService = inject(MessageService);
   username = input.required<string>();
-  messages = input.required<Message[]>();
- 
+  
   messageContent = '';
   
-  updateMessages = output<Message>();
+ 
   
   sendMessage() {
     this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
       next: message => {
-        this.updateMessages.emit(message);
+       
         this.messageForm?.reset();
       }
     })
