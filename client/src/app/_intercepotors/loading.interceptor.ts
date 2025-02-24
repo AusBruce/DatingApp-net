2ef7c043dc0ae9bb9510e1d934a45e22d302857c
AusBruce <wangyuheng086@gmail.com>
2025-02-24 18:37:10 +1100

    add fallback and change to SQLServer

--- client/src/app/_intercepotors/loading.interceptor.ts
+++ client/src/app/_intercepotors/loading.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { BusyService } from '../_services/busy.service';
 import { inject } from '@angular/core';
-import { finalize,delay } from 'rxjs';
+import { finalize,delay, identity } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   
@@ -10,7 +11,7 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   busyService.busy();
 
  return next(req).pipe(
-  delay(1000),
+(environment.production ? identity : delay(1000)),
 
   finalize(()=>{
     busyService.idle()
